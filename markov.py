af80c5e1ea4248851586ec250d1309ff354662ba
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-10 17:49:10 -0800

    Added n-gram compatibility

--- markov.py
+++ markov.py
@@ -2,6 +2,16 @@ from random import choice
 
 import sys
 
+while True:
+    try:
+        print 'How long would you like your n-grams to be?'
+        n = int(raw_input('> '))
+        break
+    except ValueError:
+        print "Please input a valid integer."
+        continue
+
+
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
 
@@ -14,7 +24,7 @@ def open_and_read_file(file_path):
     return open_file.read()
 
 
-def make_chains(text_string):
+def make_chains_bi_gram(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -55,25 +65,58 @@ def make_chains(text_string):
     else:
         chains[second_to_last_key] = [words[0]]
 
+    print chains
 
     return chains
 
 
-def make_text(chains):
+def make_chains_n_gram(text_string, n):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    chains = {}
+
+    words = text_string.split()
+
+    for i in range(len(words) - n):
+        key = tuple(words[i:i+n])
+
+        if key in chains:
+            chains[key].append(words[i+n])
+        else:
+            chains[key] = [words[i+n]]
+
+    return chains
+
+
+
+def make_text(chains, n):
     """Takes dictionary of markov chains; returns random text."""
 
     key = choice(chains.keys())
 
-    text = key[0] + ' ' + key[1]
+    text = key[0]
 
-    while key in chains: # or len(text) >= 200:
+    for i in range(1,len(key)):
+        text = text + ' ' + key[i]
+
+    while key in chains and len(text) <= 200:
         random_word = choice(chains[key])
         text = text + ' ' + random_word
 
-        if len(text) >= 200:
-            break
-
-        key = tuple([key[1], random_word])
+        key_list = list(key)[1:]
+        key_list.append(random_word)
+        
+        key = tuple(key_list)
 
     return text
 
@@ -84,10 +127,10 @@ input_path = sys.argv[1]
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains_n_gram(input_text, n)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n)
 
 print random_text
 
